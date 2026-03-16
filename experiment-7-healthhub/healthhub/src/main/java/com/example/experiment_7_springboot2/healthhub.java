7ce21701fc0515596dab6f30bc3b515e6ab56112
ishivanshuranjan <23bcs10193@cuchd.in>
2026-03-16 16:50:00 +0530

    Experiment 7: HealthHub enhancement

---
+++ experiment-7-healthhub/healthhub/src/main/java/com/example/experiment_7_springboot2/healthhub.java
@@ -0,0 +1,13 @@
+package com.example.experiment_7_springboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class healthhub {
+
+    public static void main(String[] args) {
+        SpringApplication.run(healthhub.class, args);
+    }
+
+}
