a05aee8abcc5bb10ee4a878e2b42d81dde74eaa0
ishivanshuranjan <23bcs10193@cuchd.in>
2026-03-31 23:45:42 +0530

    Remove Experiment 8 (fix submodule issue)

--- experiment-8-3rdPartyAPI/payment-mail-integration/src/main/java/com/example/paymentmailintegration/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.paymentmailintegration.repository;
-
-import com.example.paymentmailintegration.entity.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
